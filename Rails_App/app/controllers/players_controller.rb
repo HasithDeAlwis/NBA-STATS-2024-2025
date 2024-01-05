aef297239559a92b403fc10294a17cc87e12466d
HasithDeAlwis <hasithde24@gmail.com>
2024-01-05 18:50:12 -0500

    set up some routes

--- Rails_App/app/controllers/players_controller.rb
+++ Rails_App/app/controllers/players_controller.rb
@@ -1,2 +1,8 @@
+require 'csv'
+csv_file_path = '/csv-file/all_players_stats.csv'
+
 class PlayersController < ApplicationController
+    def add_all()
+        CSV.foreach(csv_file_path, headers: true) do |row|
+            print(row['id'])
 end
--- Rails_App/config/routes.rb
+++ Rails_App/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :players
   get "/search", to: "players#search", as: :search
+  post "/add_all", to:"players#add_all", as: :add_all
 end
